94bf0c2e58b144e692484f653fa2ae0c7ef54d9b
Mihkel Oviir <mihkel.oviir@fleetcomplete.eu>
2019-09-05 11:27:19 +0300

    deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   lintOnSave: 'error',
-  // runtimeCompiler: process.env.NODE_ENV !== 'production'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/foss4g2019/'
+    : '/'
 }
